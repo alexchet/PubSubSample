734aa543a13e6a0b3284e8bba7605b7dfdc4dbd1
Alex Chetcuti <chetcuti.alex@gmail.com>
2020-09-15 00:28:26 +0200

    Add project files.

---
+++ PubSubSample.Contracts/SampleEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PubSubSample.Contracts
+{
+    public class SampleEvent
+    {
+        DateTime created { get; set; }
+        string text { get; set; }
+    }
+}
---
+++ PubSubSample.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PubSubSample.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
